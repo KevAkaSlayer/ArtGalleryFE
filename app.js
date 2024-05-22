e8f7a7f46a580802d07a4db62461a3041a8e7cdc
KevAkaSlayer <pukman41@gmail.com>
2024-05-22 23:26:33 +0600

    60% done

---
+++ app.js
@@ -0,0 +1,89 @@
+const loadArtwork = () => {
+    fetch("http://127.0.0.1:8000/api/artwork-list/")
+      .then((res) => res.json())
+      .then((data) => displayArtwork(data));
+  };
+  
+  const displayArtwork = (artworks) => {
+    artworks.forEach((artwork) => {
+      const parent = document.getElementById("artwork-container");
+      const div = document.createElement("div");
+      div.classList.add("artwork-card");
+      div.classList = "border rounded-md";
+      div.innerHTML = `
+         <img src="${artwork.image}" alt="artwork" class="w-full h-40 object-cover rounded-md" />
+         <div class="px-2 py-1 flex flex-col items-start justify-start space-y-2">
+        <h3 class="text-lg font-semibold">${artwork.title}</h3>
+              <p class="text-sm pt-2">
+               ${artwork.description.slice(0, 100)}
+              </p>
+              <button class="bg-[#42A9D0] px-[5px] py-[3px] text-white rounded-md"> <a target="_blank" href="artworkdetails.html">Details</a> </button>
+              </div>
+          `;
+      parent.appendChild(div);
+    });
+  };
+
+
+
+//   const loadArtists = (search) => {
+//     document.getElementById("artists").innerHTML = "";
+//     fetch(
+//       `https://wellness-oasis-clinic-api.onrender.com/doctors/list/?search=${search ? search : ""
+//       }`
+//     )
+//       .then((res) => res.json())
+//       .then((data) => {
+//         displayDoctors(data);
+//       });
+//   };
+//   const displayDoctors = (doctors) => {
+//     doctors?.forEach((doctor) => {
+//       const parent = document.getElementById("doctors");
+//       const div = document.createElement("div");
+//       div.classList.add("doc-card");
+//       div.classList = "border rounded-md";
+//       div.innerHTML = `
+//           <img class="doc-img rounded-t-md" src=${doctor.image} alt="" />
+//               <div class="px-2 py-1 flex flex-col items-start justify-start space-y-2">
+//               <h4 class="font-semibold text-lg mt-4">${doctor?.user}</h4>
+//               <div class="flex flex-row items-start justify-start gap-1">
+//               <p class="bg-gray-300 px-[3px] py-[2px] text-xs w-fit rounded">
+//               ${doctor?.designation[0]}
+//               </p>
+//               <p class="bg-gray-300 px-[3px] py-[2px] text-xs w-fit rounded">
+              
+//               ${doctor?.specialization?.map((item) => {
+//         return `<button>${item}</button>`;
+//       })}
+//               </p>
+//               </div>
+//               <p class="text-xs pb-5">
+//                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+//                 numquam!
+//               </p>
+             
+              
+  
+//               <button class="bg-[#42A9D0] px-[5px] py-[3px] text-white rounded-md"> <a target="_blank" href="docdetails.html?doctorId=${doctor.id
+//         }">Details</a> </button>
+//               </div>
+//           `;
+  
+//       parent.appendChild(div);
+//     });
+//   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+  loadArtwork();
---
+++ authentication.js
@@ -0,0 +1,153 @@
+const getValue = (id) => {
+    const value = document.getElementById(id).value;
+    return value;
+  };
+const handleRegistration = (event) => {
+    event.preventDefault();
+    const username = getValue("username");
+    const email = getValue("email");
+    const password = getValue("password");
+    const password2 = getValue("confirm_password");
+    const bio = getValue("bio");
+    const profile_pic = getValue("profile_picture");
+    
+
+    const info = {
+      username,
+      email,
+      password,
+      password2,
+      bio,
+      profile_pic,
+      
+    };
+    console.log(JSON.stringify(info));
+
+    fetch('http://127.0.0.1:8000/api/register/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(info),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        if (data.status === 1) {
+          alert('Registration successful');
+          window.location.href = `login.html`;
+        }
+        else{
+          alert('Registration failed');
+         window.location.href = `./index.html`;
+      }
+    })
+    .catch((err) => {
+        console.log('inside err');
+        console.log(err);
+    });
+};
+
+
+// const handleLogin = (event) => {
+//     event.preventDefault();
+//     const username = getValue("login-username");
+//     const password = getValue("login-password");
+  
+//     if ((username, password)) {
+//       fetch("http://127.0.0.1:8000/api/login//api/token/", {
+//         method: "POST",
+//         headers: { "content-type": "application/json" },
+//         body: JSON.stringify({ username, password }),
+//       })
+//         .then((res) => res.json())
+//         .then((data) => {
+//           console.log(data);
+  
+//           if (data.token && data.user_id) {
+//             localStorage.setItem("token", data.token);
+//             localStorage.setItem("user_id", data.user_id);
+//             window.location.href = "index.html";
+//           }
+//         });
+//     }
+  // };
+
+  
+
+const handleLogin = (event) => {
+  event.preventDefault();
+  const username = getValue("login-username");
+  const password = getValue("login-password");
+
+  const info = {
+    username,
+    password,
+};
+
+  console.log(JSON.stringify(info));
+
+  fetch("http://127.0.0.1:8000/api/login/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(info),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("inside data");
+      console.log(data);
+      if (data.status === 1) {
+        console.log("Login successful");
+        localStorage.setItem("user_id", data.user_id);
+        localStorage.setItem("refresh", data.refresh);
+        localStorage.setItem("access", data.access);
+        localStorage.setItem("username", data.username);
+        window.location.href = `./index.html`;
+      } else {
+        console.log("Login failed");
+        alert("Login failed");
+      }
+    })
+    .catch((err) => {
+      console.log("inside err");
+      console.log(err);
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+const handleLogout = () => {
+  const refresh_token = localStorage.getItem("refresh");
+  console.log(refresh_token);
+ 
+ const info = {
+     refresh_token
+ };
+
+  console.log(JSON.stringify(info));
+ fetch('http://127.0.0.1:8000/api/logout/', {
+     method: 'POST',
+             headers: { 'Content-Type': 'application/json' },
+             body: JSON.stringify(info),  
+ })
+ .then((res) => res.json())
+ .then((data) => {
+     console.log(data);
+         
+     })
+         .catch((err) => {  
+         });
+
+
+
+        localStorage.clear();
+         window.location.href=`./login.html`;
+        
+};
+
